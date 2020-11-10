b54fb564752592e93d63da20e53a61b8ec9a13ec
sanderdebr <sander606@gmail.com>
2020-11-10 15:39:31 +0100

    first

---
+++ Pokedex/app/src/main/java/io/pokedex/Pokemon.java
@@ -0,0 +1,19 @@
+package io.pokedex;
+
+public class Pokemon {
+    private String name;
+    private String url;
+
+    Pokemon(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
